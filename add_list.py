05187aba45ab989869333dcfe9169dd018ff0b46
asgardecz <vanonvag@gmail.com>
2025-01-23 13:46:14 +0400

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,8 @@
+title_list = []
+title1 = input("Введите первый заголовок - ")
+title2 = input("Введите второй заголовок - ")
+title3 = input("Введите третий заголовок - ")
+title_list.append(title1)
+title_list.append(title2)
+title_list.append(title3)
+print(title_list)
