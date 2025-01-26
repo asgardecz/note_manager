af3e0f1a598c8b2863a033fa822c53659653c546
Ivan <vanonvag@gmail.com>
2025-01-26 17:00:00 +0400

    rem+check_zag

---
+++ update_status.py
@@ -0,0 +1,6 @@
+status_note = ['выполняется','готово','просрочено']
+status = input('Введите статус заметки в формете "выполняется","готово","просрочено" - ')
+if status in status_note:
+    print('status zametki izmenen na', status)
+else:
+    print('ne verno zadano znachenie statusa')
