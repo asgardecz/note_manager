5c087ba095650c3ea0de8beeea5b2c9d390a2c3d
Ivan <vanonvag@gmail.com>
2025-01-26 17:04:59 +0400

    check_status_tuple

--- update_status.py
+++ update_status.py
@@ -1,4 +1,4 @@
-status_note = ['выполняется','готово','просрочено']
+status_note = ('выполняется','готово','просрочено')
 status = input('Введите статус заметки в формете "выполняется","готово","просрочено" - ')
 if status in status_note:
     print('status zametki izmenen na', status)
