dcd4903477ab6315cc03d6a61467136d1954b60e
Ivan <vanonvag@gmail.com>
2025-01-27 12:50:45 +0400

    check_status_v1.2.2

--- update_status.py
+++ update_status.py
@@ -1,6 +1,21 @@
 status_note = ('выполняется','готово','просрочено')
-status = input('Введите статус заметки в формете "выполняется","готово","просрочено" - ')
-if status in status_note:
-    print('status zametki izmenen na', status)
-else:
-    print('ne verno zadano znachenie statusa')
+i = 0
+status = status_note[i]
+print('Текущий статус заметки - "выполняется"')
+print('Обновите статус заметки(либо Выполняется, Готово, Просрочено), (либо в формате 1, 2, 3)')
+while True:
+    status_change = input()
+    if status_change.lower() in status_note:
+        status = status_change
+        print('Статус обновлен -',status)
+        break
+
+    else:
+        if len(status_change) == 1:
+            if status_change == '1' or '2' or '3':
+                status_change = int(status_change)
+                status = status_note[status_change - 1]
+                print('Статус обновлен -',status)
+                break
+        else:
+            print('не верно задан формат статуса')
